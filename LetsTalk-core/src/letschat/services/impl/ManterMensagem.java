693d236facb7a3830614e9392bc76412e99ca1a1
Administrador <Administrador@user.mshome.net>
2016-09-27 19:28:13 +0000

    PAI NOSSO DOS JAVAO SOCCORE AKI

--- LetsTalk-core/src/letschat/services/impl/ManterMensagem.java
+++ LetsTalk-core/src/letschat/services/impl/ManterMensagem.java
@@ -8,6 +8,7 @@ import letschat.util.Mensagem;
 public class ManterMensagem implements IManterMensagem<Mensagem> {
 
     @Override
+    // tretas tretosas feitas durante o tempo desse comentario pra ver se o committ funfa
     public Mensagem ReceberMensagem(Mensagem ent) {
         IMensagemDAO msg = new MensagemDAO();
         return (Mensagem) msg.ReceberMsg();
