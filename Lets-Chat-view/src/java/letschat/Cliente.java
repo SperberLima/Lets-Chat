94c4835d15cbaccef15a47433c24b767d53e0055
SperberLima <jlima485@gmail.com>
2016-09-27 16:01:22 -0300

    Add files via upload

--- Lets-Chat-view/src/java/letschat/Cliente.java
+++ Lets-Chat-view/src/java/letschat/Cliente.java
@@ -1,12 +1,13 @@
 package letschat;
 
+import letschat.proxy.IChat;
 import letschat.proxy.impl.ClienteStub;
 
 public class Cliente {
 
     public static void main(String[] args) {
         
-        ClienteStub eu = new ClienteStub("localhost", 2223);
+        IChat eu = new ClienteStub("localhost", 2223);
         
     }
 }
--- Lets-Chat-view/src/java/letschat/servlet/CGCriar.java
+++ Lets-Chat-view/src/java/letschat/servlet/CGCriar.java
@@ -1,5 +1,20 @@
 package letschat.servlet;
 
-public class CGCriar {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CGCriar extends HttpServlet {
+
+    private String jsp = "jsp/erro.jsp";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
     
+        
+        
+    }
 }
